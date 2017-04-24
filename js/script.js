e7b2467e072ac0292f0389918fb0f3f529ca2a94
halo2008 <ksedkowski@gmail.com>
2017-04-24 18:59:15 +0200

    dodanie komentarza

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 var name = prompt('Wpisz swoje imie');
-alert('Witaj, ' + name);
+//alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+/* komentowanie jest takie samo jak w C czy C++ */
+// i o to chodzi ;) 
